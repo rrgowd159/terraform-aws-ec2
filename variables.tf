47095153bec40d38749d2e85d355c0e6ce9e2fd7
rakeshgowd100 <rakeshgowd100@gmail.com>
2024-11-17 20:31:35 +0530

    second

--- variables.tf
+++ variables.tf
@@ -1,33 +1,32 @@
 variable "ec2_ami" {
   description = "This is ec2 instance"
-  type = string
-  default = "ami-063d43db0594b521b"
+  type        = string
+  # default     = "ami-063d43db0594b521b" # Default AMI for the EC2 instance
 }
 
 variable "vpc_tags" {
   description = "This is tags for ec2"
-  type = map(string)
-  default = {
-    "Name" = "My_EC2"
-    "Env" = "Test"
-  }
+  type        = map(string)
+  # default     = {
+  #   "Name" = "My_EC2" # Default name tag for the EC2 instance
+  #   "Env"  = "Test"   # Default environment tag set to 'Test'
+  # }
 }
 
 variable "ec2_type" {
   description = "This is instance type"
-  type = string
-  default = "t2.micro"
+  type        = string
+  # default     = "t2.micro" # Default instance type is 't2.micro'
 }
 
-
 variable "ec2_zone" {
-  description = "This is avaliablity zone"
-  type = string
-  default = "us-east-1a"
+  description = "This is availability zone"
+  type        = string
+  # default     = "us-east-1a" # Default availability zone set to 'us-east-1a'
 }
 
 variable "ec2_count" {
-  description = "this is count"
-  type = number
-  default = 1
+  description = "This is count"
+  type        = number
+  # default     = 1 # Default count of EC2 instances is 1
 }
